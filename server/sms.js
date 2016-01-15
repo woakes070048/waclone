7a60deed55353515e6047588ed3bf8f03ab18a96
Nick Roberts <nick.roberts@utah.edu>
2016-01-15 13:22:20 -0700

    init

--- server/sms.js
+++ server/sms.js
@@ -1,4 +1,12 @@
-if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
-  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
-  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+
+if (Meteor.isServer) {
+
+  if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+  	SMS.twilio = {
+	  ACCOUNT_SID: Meteor.settings.TWILIO.SID,
+	  AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
+	};
+    Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+    Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+  }
 }
