0b52f8c5a84cb6d1e723421c4438d3db718eab5e
Nick Roberts <nick.roberts@utah.edu>
2016-01-15 13:23:14 -0700

    init

--- server/sms.js
+++ server/sms.js
@@ -3,6 +3,7 @@ if (Meteor.isServer) {
 
   if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
   	SMS.twilio = {
+  	  FROM: Meteor.settings.TWILIO.FROM,	
 	  ACCOUNT_SID: Meteor.settings.TWILIO.SID,
 	  AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
 	};
